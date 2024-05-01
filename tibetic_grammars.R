5538feb3cdab6b160e0a095e73cf6e7515f6bc6f
Maaz Shaikh <maazahma@ualberta.ca>
2024-05-01 16:48:15 -0600

    Plots for Methodology

--- tibetic_grammars.R
+++ tibetic_grammars.R
@@ -1,12 +1,19 @@
 setwd("D:/01_Projects/01_PhD/03_Analysis/02_TGR/tibetic_grammars")
 
+# Print current working directory
+print(getwd())
+
+# List files in the current directory
+list.files()
+
+
 library(readxl)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 
 # Load the data, assuming the first row is now properly set as column names
-tibetic_grammars <- read_excel("Tibetic_Grammars_Review_Master.xlsx", skip = 0)
+tibetic_grammars <- read_excel("Tibetic-Grammars_Review_Master.xlsx")
 
 # Define the desired order for branches
 branch_order <- c("NW", "W", "C", "SW", "S", "SE", "E", "NE")
@@ -142,5 +149,100 @@ venn.plot <- venn.diagram(
 # Plot the diagram
 grid.draw(venn.plot)
 
+###
+#Methodology Likert Scale Scores by Publication Period and Type: Box Plot
+###
+#############
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+# Load the data
+tibetic_grammars <- read_excel("Tibetic-Grammars_Review_Master.xlsx")
+
+# Ensure that 'period' and 'is_diss' columns are correctly formatted as factors
+tibetic_grammars <- tibetic_grammars %>%
+  mutate(
+    period = factor(period, levels = c("E", "M", "L"), labels = c("Early", "Middle", "Late")),
+    is_diss = ifelse(`is_diss` == "Y", "Dissertation", "Published Grammar"),
+    is_diss = factor(is_diss, levels = c("Dissertation", "Published Grammar")),
+    `methodology_LS` = as.numeric(`methodology_LS`)  # Ensure this is numeric for plotting
+  )
+
+# Double-check data entries for each category
+print(table(tibetic_grammars$is_diss))
+
+# Create a box plot
+ggplot_object <- ggplot(tibetic_grammars, aes(x = period, y = `methodology_LS`, fill = is_diss)) +
+  geom_boxplot(position = position_dodge2(preserve = "single"), width = 1) +  # Set a fixed width for all boxes
+  scale_fill_manual(values = c("Dissertation" = "lightblue", "Published Grammar" = "lightgreen"),
+                    name = "Type of grammar",
+                    labels = c("Dissertation", "Published grammar")) +
+  labs(
+    x = "Period of publishing/defense",
+    y = "Average Likert Scale score"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(size = 12, face = "bold"),  # Increase font size for x-axis labels
+    axis.title.x = element_text(size = 12, face = "bold", margin = margin(t = 10)),  # Increase space between axis labels and title
+    axis.text.y = element_text(size = 12, face = "bold"),
+    axis.title.y = element_text(size = 12, face = "bold", margin = margin(r = 10)),  # Increase space between axis labels and title
+  )
+
+# Print the plot
+print(ggplot_object)
 
 
+###################
+#Methodology LS versus the count of grammars
+###################################
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+# Load the data
+tibetic_grammars <- read_excel("Tibetic-Grammars_Review_Master.xlsx")
+
+# Preprocessing the data
+tibetic_grammars <- tibetic_grammars %>%
+  mutate(
+    is_diss = ifelse(`is_diss` == "Y", "Dissertation", "Published Grammar"),
+    is_diss = factor(is_diss, levels = c("Dissertation", "Published Grammar")),
+    `methodology_LS` = as.numeric(`methodology_LS`)  # Ensure this is numeric for grouping
+  )
+
+# Double-check data entries for each category
+print(table(tibetic_grammars$is_diss))
+
+# Creating a summary table for plotting
+likert_counts <- tibetic_grammars %>%
+  group_by(`methodology_LS`, is_diss) %>%
+  summarise(count = n(), .groups = 'drop')
+
+# Double-check data entries for each category
+print(table(tibetic_grammars$is_diss))
+
+# Create a bar plot
+bar_plot <- ggplot(likert_counts, aes(x = as.factor(`methodology_LS`), y = count, fill = is_diss)) +
+  geom_bar(stat = "identity", position = position_dodge2(preserve = "single", width = 0.9), width = 0.5) +  # Adjust bar width and dodge width
+  scale_fill_manual(values = c("Dissertation" = "lightblue", "Published Grammar" = "lightgreen"),
+                    name = "Type of grammar",
+                    labels = c("Dissertation", "Published grammar")) +
+  labs(
+    x = "Methodology (Likert score)",
+    y = "Count of grammars"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "top",
+    axis.text.x = element_text(size = 12, face = "bold"),
+    axis.title.x = element_text(size = 12, face = "bold", margin = margin(t = 10)),
+    axis.text.y = element_text(size = 12, face = "bold"),
+    axis.title.y = element_text(size = 12, face = "bold", margin = margin(r = 10))
+  )
+
+# Print the plot
+print(bar_plot)
